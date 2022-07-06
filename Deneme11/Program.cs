80415aedac75f47041ec528427c5750e48e2d8f5
sdestur <sdestur05@gmail.com>
2022-07-06 13:47:42 +0300

    Add project files.

---
+++ Deneme11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DortIslem dortIslem = new DortIslem();
+            dortIslem.Topla(11,256);
+           
+        }
+    }
+}
